68780121754572ef027b9d736114c89a59686b9a
Chylle Miles <chyllemiles@Chylles-iMac.local>
2021-11-28 21:40:58 -0600

    updated build:

---
+++ build/static/js/react-syntax-highlighter_languages_refractor_hsts.6045b74a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonporb-defence-react"]=this["webpackJsonporb-defence-react"]||[]).push([[55],{284:function(e,a,s){"use strict";function t(e){e.languages.hsts={directive:{pattern:/\b(?:max-age=|includeSubDomains|preload)/,alias:"keyword"},safe:{pattern:/\d{8,}/,alias:"selector"},unsafe:{pattern:/\d{1,7}/,alias:"function"}}}e.exports=t,t.displayName="hsts",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hsts.6045b74a.chunk.js.map
---
+++ build/static/js/react-syntax-highlighter_languages_refractor_ini.d52d134b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonporb-defence-react"]=this["webpackJsonporb-defence-react"]||[]).push([[60],{289:function(t,e,n){"use strict";function s(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=s,s.displayName="ini",s.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.d52d134b.chunk.js.map
